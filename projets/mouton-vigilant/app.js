ebd677a7cc61aeca38a72ac19ae9f6496092516e
fr1255 <76732336+fr1255@users.noreply.github.com>
2026-06-24 23:33:13 +0200

    Update app.js

--- projets/mouton-vigilant/app.js
+++ projets/mouton-vigilant/app.js
@@ -592,21 +592,3 @@ afficherMedicaments();
 ouvrirDepuisNotification();
 afficherParametresMedicaments();
 
-// ==============================
-// OneSignal
-// ==============================
-
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-
-OneSignalDeferred.push(async function (OneSignal) {
-
-  await OneSignal.init({
-    appId: "33afb21b-f145-4372-ae00-b7f5f656e025",
-    notifyButton: {
-      enable: false
-    }
-  });
-
-  console.log("✅ OneSignal initialisé");
-
-});
