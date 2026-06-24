47653891e662a576612e91f60d6deb03e257055f
fr1255 <76732336+fr1255@users.noreply.github.com>
2026-06-24 14:51:01 +0200

    Initialize OneSignal and handle notification permission

--- projets/mouton-vigilant/onesignal.js
+++ projets/mouton-vigilant/onesignal.js
@@ -1,1 +1,19 @@
-alert("onesignal.js chargé !");
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+OneSignalDeferred.push(async function (OneSignal) {
+  alert("OneSignal démarre");
+
+  await OneSignal.init({
+    appId: "33afb21b-f145-4372-ae00-b7f5f656e025"
+  });
+
+  alert("OneSignal initialisé");
+
+  const bouton = document.getElementById("activerNotifications");
+
+  bouton.onclick = async function () {
+    alert("Bouton cliqué");
+    await OneSignal.Notifications.requestPermission();
+    alert("Permission : " + OneSignal.Notifications.permission);
+  };
+});
